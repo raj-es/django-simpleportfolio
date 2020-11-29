c7855cb2df8bc25d89e596d5a78e67b5eaceb247
Rajesh <rajeshkarki500@gmail.com>
2020-11-29 18:26:13 +0545

    All files are uploaded

---
+++ home/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from home import views
+# Django admin Header Custumization
+admin.site.site_header = "Login to Developer Rajesh"
+admin.site.site_title = "Welcome "
+admin.site.index_title = "Welcome to my portal"
+
+urlpatterns = [
+
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('projects', views.projects, name='projects'),
+    path('contact', views.contact, name='contact')
+]
